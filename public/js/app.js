10e3d202524077948aef74c9ab8381c16a0c9dc5
hoale <ledanghoa1984@outlook.com>
2016-03-22 22:32:45 +0700

    add notification

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,24 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anomyous';
+var room = getQueryVariable('room') || 'Anomyous';
+jQuery('.room-title').text(room);
+console.log(name + ' ' + room);
 socket.on('connect', function () {
     console.log('connected to socket io server');
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
+    })
 });
 
 socket.on('message', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);
+    var $message = jQuery('.message');
     console.log('New message:');
     console.log(message.text);
-    jQuery('.message').append('<p><strong>'+ momentTimestamp.local().format("h:mm a :") +'</strong> ' + message.text + '</p>')
+    $message.append('<p><strong>' +message.name + ' ' + momentTimestamp.local().format("h:mm a ") +'</strong> </p>')
+    //$message.append('<p><strong>' + momentTimestamp.local().format("h:mm a :") + '</strong> ' + message.text + '</p>')
+    $message.append('<p>'+message.text+'</p>');
 });
 
 var $form = jQuery('#message-form');
@@ -15,6 +26,7 @@ $form.on('submit', function (event) {
     var message = $form.find('input[name=message]');
     event.preventDefault();
     socket.emit('message', {
+        name: name,
         text: message.val()
     });
     message.val('');
--- server.js
+++ server.js
@@ -5,21 +5,45 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 app.use(express.static(__dirname + '/public'));
-
+var clientInfo = {};
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
 
+    socket.on('disconnect', function(){
+        if(typeof clientInfo[socket.id] !== 'undefined'){
+            var userData = clientInfo[socket.id];
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message',{
+                name: 'System',
+                text: userData.name + ' ' + 'has left the room!',
+                timestamp: moment().valueOf()
+            });
+            delete  clientInfo[socket.id];
+        }
+    });
+
+    socket.on('joinRoom', function (req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' ' + 'has joined!',
+            timestamp: moment().valueOf()
+        })
+    });
+
     socket.on('message', function (message) {
-        console.log('Message receive: '+ message.text );
+        console.log('Message receive: ' + message.text);
         //emit will not show not on my own
         //socket.broadcast.emit('message', message);
         message.timestamp = moment().valueOf();
-        io.emit('message', message);
+        io.to(clientInfo[socket.id].room).emit('message', message);
     });
 
     socket.emit('message', {
+        name: 'System',
         text: 'Server: Welcome to the chat application',
-        timestamp:moment().valueOf()
+        timestamp: moment().valueOf()
     });
 });
 
